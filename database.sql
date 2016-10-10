6eaba580fbd8edbe4626eb9572d5ffff01efa70b
Pedro Bucho <pedro@bucho.pt>
2016-10-10 22:00:54 +0100

    Updated SQL query for table creation

--- database.sql
+++ database.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `feeds` (
   `title` varchar(255) NOT NULL,
   `link` varchar(255) NOT NULL,
   `description` mediumtext NOT NULL,
+  `href` varchar(255) NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -12,7 +13,9 @@ CREATE TABLE IF NOT EXISTS `feed_items` (
   `link` varchar(255) NOT NULL,
   `description` longtext NOT NULL,
   `pubDate` datetime DEFAULT NULL,
+  `offset` varchar(5) NOT NULL DEFAULT '+0000',
   `guid` varchar(255) DEFAULT NULL,
+  `isPermaLink` tinyint(1) NULL DEFAULT NULL,
   `feed` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`feed`) REFERENCES `feeds` (`id`)
