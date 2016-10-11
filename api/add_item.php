3c520b2ce8044d17a042f3e6a54267144eb317e6
pbucho <pedro.bucho@gmail.com>
2016-10-11 15:08:17 +0100

    Created token generation and authentication

--- api/add_item.php
+++ api/add_item.php
@@ -1,12 +1,12 @@
 <?php
   include_once("funcs/func_add_item.php");
   echo api_add_item(isset($_POST['token']) ? $_POST['token'] : null,
-		    isset($_POST['id']) ? $_POST['id'] : null,
-                    isset($_POST['title']) ? $_POST['title'] : null,
-                    isset($_POST['link']) ? $_POST['link'] : null,
-                    isset($_POST['description']) ? $_POST['description'] : null,
-                    isset($_POST['pubDate']) ? $_POST['pubDate'] : null,
-                    isset($_POST['offset']) ? $_POST['offset'] : null,
-                    isset($_POST['guid']) ? $_POST['guid'] : null,
-                    isset($_POST['isPermaLink']) ? $_POST['isPermaLink'] : null);
+					isset($_POST['id']) ? $_POST['id'] : null,
+					isset($_POST['title']) ? $_POST['title'] : null,
+					isset($_POST['link']) ? $_POST['link'] : null,
+					isset($_POST['description']) ? $_POST['description'] : null,
+					isset($_POST['pubDate']) ? $_POST['pubDate'] : null,
+					isset($_POST['offset']) ? $_POST['offset'] : null,
+					isset($_POST['guid']) ? $_POST['guid'] : null,
+					isset($_POST['isPermaLink']) ? $_POST['isPermaLink'] : null);
 ?>
